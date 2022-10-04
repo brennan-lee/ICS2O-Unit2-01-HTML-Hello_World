292a509dbb79beebc025f501a9ad29a3cf273f88
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-10-04 14:21:02 -0400

    try

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 Brennan Lee All rights reserved
+//
+// Created by: Brennan Lee
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+/**
+ * This function displays "Hello, World!".
+ * @constructor
+ */
+
+function myButtonClicked() {
+    document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  }
